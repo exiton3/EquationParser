5a59d28eb9a28a7001b207d23da9254e8a3e4b82
exiton3 <exiton3@gmail.com>
2016-10-05 21:19:22 +0300

    tests

--- EquationSimplifier/EquationSimplifier.Tests/TokenizerTests.cs
+++ EquationSimplifier/EquationSimplifier.Tests/TokenizerTests.cs
@@ -1,12 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using NUnit.Framework;
+
+using static System.Console;
+
 
 namespace EquationSimplifier.Tests
 {
+    [TestFixture]
     public class TokenizerTests
     {
+
+        [Test]
+        public void CanParse()
+        {
+            string equationString = "x^2 + 3.5xy + y = y^2 - xy + y";
+
+            var replace = equationString.Replace(" ", "");
+            var strings = replace.Split('=');
+            string leftPart = strings[0];
+            string rightPart = strings[1];
+
+            var s1 = leftPart.Split(new[] {"+", "-"}, StringSplitOptions.RemoveEmptyEntries);
+
+
+            WriteLine(rightPart);
+        }
     }
 }
