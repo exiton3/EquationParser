6dd51aa354f33b601d6ca4e890a64722de4b18e3
exiton3 <exiton3@gmail.com>
2016-10-14 17:46:22 +0300

    deleted unnecesary files

--- EquationSimplifier/EquationSimplifier/IContext.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace EquationSimplification
-{
-	public interface IContext
-	{
-		double Lookup(string s);
-		void Assign(string name, double value);
-	}
-}
--- EquationSimplifier/EquationSimplifier/Lex/Token.cs
+++
@@ -1,8 +0,0 @@
-namespace EquationSimplification.Lex
-{
-    public class Token
-    {
-        public TokenKind Name { get; set; }
-        public string Value { get; set; }
-    }
-}
